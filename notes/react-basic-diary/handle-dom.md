868432a765e7e0e55171d35e6d055f38c8581d47
choidoorim <doorim321@naver.com>
2022-08-15 22:06:03 +0900

    add: useRef

--- notes/react-basic-diary/handle-dom.md
+++ notes/react-basic-diary/handle-dom.md
@@ -1,1 +1,17 @@
 # React 에서 DOM 조작하기
+조건에 맞지 않은 입력이 들어왔다고해서 ```alert``` 를 통해 띄우는 것은 요새 트렌드에 벗어납니다.   
+따라서 보통 웹 사이트들은 사용하지 자주 사용하지 않고, Focus 를 사용한다.
+
+```useRef``` 는 **HTML Dom 요소에 접근할 수 있도록 도와주는 역할, 기능**을 한다.   
+```javascript
+const authorInput = useRef();
+//...
+if (state.author < 1) {
+  authorInput.current.focus();
+  return;
+}
+//...
+<input ref={authorInput} name="author" value={state.author} onChange={handleChangeState}/>
+```
+```authorInput.current.focus();``` 코드의 의미는 ```useRef()``` 로 생성한 authorInput 객체에서 현재 가리키는 값을 current 라는 Property 로 불러와서 사용할 수 있습니다.
+이 불러온 태그의 focus 라는 기능을 사용할 수 있게 하는 것입니다.
--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,10 @@
-import { useState } from 'react';
+import {useState, useRef} from 'react';
 
 const DiaryEditor = () => {
+
+  const authorInput = useRef();
+  const contentsInput = useRef();
+
   const [state, setState] = useState({
     author: 'choi',
     contents: 'Contents',
@@ -14,7 +18,16 @@ const DiaryEditor = () => {
     })
   }
 
+  // 일기저장하기 버튼을 눌렀을 때
   const handleClick = () => {
+    if (state.author.length < 1) {
+      authorInput.current.focus();
+      return;
+    }
+    if (state.contents.length < 5) {
+      contentsInput.current.focus();
+      return;
+    }
     console.log(state.author, state.contents, state.emotion);
     alert('저장 성공');
   }
@@ -23,12 +36,13 @@ const DiaryEditor = () => {
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
-        <input name="author" value={state.author} onChange={handleChangeState} />
+        <input ref={authorInput} name="author" value={state.author} onChange={handleChangeState}/>
       </div>
       <div>
-        <textarea name="contents" value={state.contents} onChange={handleChangeState}/>
+        <textarea ref={contentsInput} name="contents" value={state.contents} onChange={handleChangeState}/>
       </div>
       <div>
+        오늘의 감정점수:
         <select name="emotion" value={state.emotion} onChange={handleChangeState}>
           <option value={1}>1</option>
           <option value={2}>2</option>
